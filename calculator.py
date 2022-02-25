3d42dd97f82b9ff59bed3860e0483b9479f9be7d
Cruz Castillo <cruzcastillo8@gmail.com>
2022-02-25 01:09:47 -0600

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,38 +1,73 @@
-# Python Program to Make a Simple Calculator
-
-def multiplication(num1, num2):
-    return num1 * num2
-
+#Python program to make basic calculator. It is my final version
+# but it has errors when letters are input or numbers different to 1, 2, 3, 4, or 5 are input.
 
 def addition(num1, num2):
     return num1 + num2
 
-
-def subtraction(num2, num1):
+def subtraction(num1, num2):
     return num1 - num2
 
+def multiplication(num1, num2):
+    return num1 * num2
 
 def divide(num1, num2):
-    return num1 / num2
+    try:
+        num1 / num2
+    except ZeroDivisionError:
+        print("division by zero is not allowed, try again!")
+        print()
+    else:
+        print(value1, "/", value2, "=", value1 / value2)
+        print()
+    finally:
+        print("executing finally clause")
+        print()
+
+def is_letter(operation):   # Function does not work yet
+    operation.isalpha()
+    print()
+    print("This value cannot be letters.")
+    #break
 
 
-while True:
+finished = False
+while finished == False:
+
+    print("Hi, I am your personal calculator. I can help you with the following operations: ")
+
+    print("Select a number:  1-Addition, 2-Subtraction, 3-Multiplication, 4-Division, 5-End")
+    operation = int(input("Choose  number of operation 1, 2, 3, 4, 5 : "))
+
+
+    if operation == 5:
+        print()
+        print("Have a good day, bye.")
+        break
+
+
+    if (operation >= 1) and (operation <=5):
+        pass
+    else:
+        print()
+        print("The number of the operation must be between 1 and 5.")
+        break
+
+
     value1 = int(input("Enter 1st number: "))
     value2 = int(input("Enter 2nd number: "))
 
-    print("Select operation 1-Division, 2-Multiplication, 3-Addition, 4-Subtraction")
 
-    operation = int(input("Choose operation 1/2/3/4: "))
     if operation == 1:
-        print(value1, "/", value2, "=", divide(value1, value2))
+        print(value1, "+", value2, "=", addition(value1, value2))
+        print()
 
     elif operation == 2:
-        print(value1, "*", value2, "=", multiplication(value1, value2))
+        print(value1, "-", value2, "=", subtraction(value1, value2))
+        print()
 
     elif operation == 3:
-        print(value1, "+", value2, "=", addition(value1, value2))
-    elif operation == 4:
-        print(value1, "-", value2, "=", subtraction(value1, value2))
+        print(value1, "*", value2, "=", multiplication(value1, value2))
+        print()
 
-    else:
-        print("Enter correct operation")
+    elif operation == 4:
+        divide(value1, value2)
